81a2ab5e77b749ef3267e8bf9fcdff897f3b9a91
leekyuhwan1234 <lgh4712@gmail.com>
2021-08-20 19:48:07 +0900

    Create : cropsPercent

--- routes/dash.js
+++ routes/dash.js
@@ -8,6 +8,7 @@ router.post('/crop', dashController.dash_cropCategoryCount);
 
 router.post('/cropNum',dashController.dash_cropNumber);
 router.post('/cropWeek',dashController.dash_cropWeekDate)
+router.post('/cropPercent', dashController.dash_cropPercent)
 
 
 module.exports = router
