5feeca2df14c0d1c983acfb7b2152648e23f28bb
leekyuhwan1234 <lgh4712@gmail.com>
2021-08-20 16:31:54 +0900

    Modify : cropDAO, CropNum, CropWeek position

--- routes/dash.js
+++ routes/dash.js
@@ -2,7 +2,12 @@ var express = require('express');
 var router = express.Router();
 var dashController = require('../controllers/dashController')
 
+
 router.get('/',dashController.dash_main);
 router.post('/crop', dashController.dash_cropCategoryCount);
+
+router.post('/cropNum',dashController.dash_cropNumber);
+router.post('/cropWeek',dashController.dash_cropWeekDate)
+
 
 module.exports = router
